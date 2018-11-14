5d399b036f2b65d703efde877053d76e0598b983
Victor Alveflo <victor.alveflo@gmail.com>
2018-11-14 15:31:06 +0100

    update executable

--- bin/cargo.js
+++ bin/cargo.js
@@ -3,16 +3,25 @@
 const { resolve } = require('path');
 const { spawn } = require('child_process');
 
-const tsnode = spawn(
-  resolve(__dirname, '../node_modules', '.bin', 'ts-node.cmd'),
-  [
-    '--project',
-    resolve(__dirname, '../tsconfig.json'),
-    resolve(__dirname, '../src', 'Cli.ts'),
-  ],
-  { stdio: [process.stdin, process.stdout, process.stderr] },
-);
+// const tsnode = spawn(
+//   resolve(__dirname, '../node_modules', '.bin', 'ts-node.cmd'),
+//   [
+//     '--project',
+//     resolve(__dirname, '../tsconfig.json'),
+//     resolve(__dirname, '../src', 'Cli.ts'),
+//   ],
+//   { stdio: [process.stdin, process.stdout, process.stderr] },
+// );
 
-tsnode.on('error', data => {
-  console.log(`error: ${data}`);
+// tsnode.on('error', data => {
+//   console.log(`error: ${data}`);
+// });
+
+require('ts-node').register({
+	typeCheck: false,
+	transpileOnly: true,
+	files: true,
+	ignore: [],
 });
+
+require('../src/Cli');
---
+++ versioner.js
@@ -0,0 +1,29 @@
+if (process.argv.length < 3) {
+    console.log("patch/minor/major must be provided.");
+    return;
+}
+
+let operation = process.argv[2].toString();
+
+if (operation != "minor" && operation != "major" && operation != "patch") {
+    console.log("patch/minor/major must be provided.");
+    return;
+}
+
+const writeJsonFile = require('write-json-file');
+let pkg = require("./package.json");
+const v = pkg.version.split('.');
+
+let newVersion = v;
+if (operation === "patch") {
+    newVersion = v[0] + "." + v[1] + "." + (parseInt(v[2]) + 1);
+} else if (operation === "minor") {
+    newVersion = v[0] + "." + (parseInt(v[1]) + 1) + "." + v[2];
+} else if (operation === "major") {
+    newVersion = (parseInt(v[0]) + 1) + "." + v[1] + "." + v[2];
+}
+
+console.log("New version: ", newVersion);
+pkg.version = newVersion;
+
+writeJsonFile.sync("./package.json", pkg);
